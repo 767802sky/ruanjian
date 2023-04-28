e66dff6700e555f87961f59ea2bd7c9a8d8e3336
767802sky <85334007+767802sky@users.noreply.github.com>
2023-04-28 23:21:07 +0800

    Add files via upload
    最大连续子数组和

---
+++ c.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int nums[100000];
+int len;
+int main()
+{
+	int dp[100010] = {0};
+	int i=0;
+	int res = -10010;
+	int tool=0;
+    scanf("%d",&len);
+		for(i=0;i<len;i++){
+			scanf("%d",&nums[i]);
+			if(nums[i]>0)tool=1;
+		}
+		if(tool == 0){
+			printf("0");
+			return 0;
+		}
+        dp[0] = nums[0];
+
+        for(i=1;i<=len;i++){
+            dp[i] = (nums[i]+dp[i-1])>nums[i]? nums[i]+dp[i-1]:nums[i];
+        }
+        for( i=0;i<=len;i++)
+        {
+           if(res<dp[i])res = dp[i];
+
+        }
+	printf("%d",res);
+	system("pause");
+	return 0;
+}
